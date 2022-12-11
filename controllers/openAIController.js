a4fcfb33bfb6be6a0b427fbf50d3c3b9afdf06cd
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-12-11 22:52:42 +0700

    first commit

---
+++ controllers/openAIController.js
@@ -0,0 +1,7 @@
+const generateImage = async (req, res) => {
+    // Do something
+    res.status(200).json({
+        status: 'success',
+        message: 'Handling POST requests to /generate'
+    });
+}
